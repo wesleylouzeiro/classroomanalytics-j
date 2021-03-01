36f52e4c4f36d8185fc52c94778eb527aee41e59
Wesley Louzeiro <wesleylouzeiro@hotmail.com>
2021-03-01 00:33:51 -0300

    AAtualização e refatoração do cotrolador ConversaAnalitics.

--- src/controller/ConversaAnaliticsControllher.java
+++ src/controller/ConversaAnaliticsControllher.java
@@ -7,6 +7,7 @@ package controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import model.Pessoa;
@@ -30,12 +31,34 @@ public class ConversaAnaliticsControllher {
         return resultado;
     }
     
-    public Map<String,Pessoa> gerarListaContatos(String conversa){
+    public List<Pessoa> gerarListaContatos(String conversa){
         ProcessadorConversa processador = new ProcessadorConversa();  
         List<String> listaMenssagem = processador.extrairConversas(conversa);
-        Map<String,Pessoa> mapaContato = processador.estruturaConversas(listaMenssagem);
+        Map<String,Pessoa> mapaContato = processador.estruturaConversas(listaMenssagem);       
+        List<Pessoa> listaContatos = new ArrayList<>(mapaContato.values());
+        return listaContatos;
+    }
+    
+    /*
+     "Contados", "Interação", "Texto", "Imagens", "Documentos", "Audios", "Vídeos", "Outros"
+    */
+    public Object [][] gerarMapaEstaisticoDeContatos(String conversa){
+        List<Pessoa> listaContatos = this.gerarListaContatos(conversa);
+        ArrayList<String[]> listaDeDados = new ArrayList<>();        
+        Object [][] matrizDados = new Object [listaContatos.size()][8];                        
+        for(int index=0; index<listaContatos.size();index++){
+            Pessoa contato = listaContatos.get(index);
+            matrizDados[index][0] = contato.getContato();
+                matrizDados[index][1] = contato.getResumoConversas().getQuantidadeInteracao();
+                matrizDados[index][2] = contato.getResumoConversas().getQuantidadeTexto();
+                matrizDados[index][3] = contato.getResumoConversas().getQuantidadeImagens();
+                matrizDados[index][4] = contato.getResumoConversas().getQuantidadeDocumentos();
+                matrizDados[index][5] = contato.getResumoConversas().getQuantidadeAudios();
+                matrizDados[index][6] = contato.getResumoConversas().getQuantidadeVideos();
+                matrizDados[index][7] = contato.getResumoConversas().getQuantidadeOutros();
+        }
         
-        return mapaContato;
+        return matrizDados;
     }
     
 }
