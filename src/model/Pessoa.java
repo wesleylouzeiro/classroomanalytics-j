2e6d60299baf5b56cbd578842b6d416f5284b349
Wesley Louzeiro <wesleylouzeiro@hotmail.com>
2021-03-01 12:13:46 -0300

    Adição da função getConversasData na classe de modelo Pessoa.

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -16,14 +16,15 @@ import java.time.LocalDate;
  */
 public class Pessoa {
 
-    
     private final String nome;
     private final String contato;
     private Tipo tipo;
     private final List<Conversa> conversas = new ArrayList<>();
-    
-    public enum Tipo{ALUNO,PROFESSOR,COORDENADOR,OUTROS};
-    
+
+    public enum Tipo {
+        ALUNO, PROFESSOR, COORDENADOR, OUTROS
+    };
+
     public Pessoa(String nome, String contato) {
         this.nome = nome;
         this.contato = contato;
@@ -34,21 +35,30 @@ public class Pessoa {
         this.contato = contato;
         this.tipo = tipo;
     }
-    
-    public void setConversa(Conversa conversa){
+
+    public void setConversa(Conversa conversa) {
         this.getConversas().add(conversa);
     }
-    
+
     /**
      * @return the conversas
      */
     public List<Conversa> getConversas() {
         return conversas;
     }
-    
-    public Conversa getResumoConversas(){
+
+    public Conversa getConversasData(LocalDate data) {
+        for (Conversa conversa : conversas) {
+            if (conversa.getData().equals(data)) {
+                return conversa;
+            }
+        }
+        return null;
+    }
+
+    public Conversa getResumoConversas() {
         Conversa conversaResumida = new Conversa(LocalDate.now());
-        conversas.forEach((conversa)->conversaResumida.unirConversa(conversa));
+        conversas.forEach((conversa) -> conversaResumida.unirConversa(conversa));
         return conversaResumida;
     }
 
@@ -80,13 +90,12 @@ public class Pessoa {
         this.tipo = tipo;
     }
 
-    
     @Override
-    public boolean equals(Object obj) {        
-        if(obj!=null && obj instanceof Pessoa){
+    public boolean equals(Object obj) {
+        if (obj != null && obj instanceof Pessoa) {
             Pessoa pessoa = (Pessoa) obj;
             return (pessoa.contato.equals(this.contato) && pessoa.nome.equals(this.nome));
-        }        
+        }
         return false;
     }
 
@@ -97,13 +106,10 @@ public class Pessoa {
         hash = 71 * hash + Objects.hashCode(this.contato);
         return hash;
     }
-    
+
     @Override
     public String toString() {
         return this.contato;
     }
-    
-    
-    
-    
+
 }
